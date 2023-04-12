126916e9c1f342599791520c6a8766697aaa9d95
Robert Beglaryan <robbegarm@gmail.com>
2023-04-12 16:11:03 +0300

    Made Hero responsive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 const Hero = () => {
   const lang = useSelector((state) => state.language.activeLanguage);
-  const heroP = {
+  const heroP1 = {
     en: "Hi, my name is",
     ru: "Привет, меня зовут",
     arm: "Բարև, իմ անունն է",
@@ -13,14 +13,14 @@ const Hero = () => {
     ru: "Роберт Бегларян.",
     arm: "Ռոբերտ Բեգլարյան.",
   };
-  const heroH2 = {
-    en: "I am a passionate frontend developer.",
+  const heroP2 = {
+    en: "I am a passionate web developer.",
     ru: "Я веб разработчик.",
     arm: "Ես վեբ ծրագրավորող եմ:",
   };
-  const heroH3 = {
-    en: "I have experience in designing, building, and deploying real-world websites. I create engaging, responsive, and user-friendly interfaces. I am open for freelance opportunities.",
-    ru: "У меня есть опыт проектирования, создания и развертывания реальных веб-сайтов. Я создаю привлекательные, отзывчивые и удобные интерфейсы. Я открыт для фриланса.",
+  const heroP3 = {
+    en: "I have experience in designing, building, and deploying websites. I create engaging, responsive, and user-friendly interfaces. I am open for freelance opportunities.",
+    ru: "У меня есть опыт проектирования, создания и развертывания веб-сайтов. Я создаю привлекательные, отзывчивые и удобные интерфейсы. Я открыт для фриланса.",
     arm: "Ես վեբ կայքերի դիզայնի, կառուցման և տեղադրման փորձ ունեմ: Ես ստեղծում եմ գրավիչ, responsive և օգտատերի համար հարմար ինտերֆեյսներ: Ես բաց եմ ֆրիլանսի առաջարկների համար:",
   };
   const heroCta = {
@@ -31,12 +31,12 @@ const Hero = () => {
 
   return (
     <div className={classes["hero"]}>
-      {/* <div className={classes["hero--bg"]}></div> */}
+      <div className={classes["hero--bg"]}></div>
       <div className={classes["hero--container"]}>
-        <p>{heroP[lang]}</p>
+        <p className={classes["hero--p1"]}>{heroP1[lang]}</p>
         <h1>{heroH1[lang]}</h1>
-        <h2>{heroH2[lang]}</h2>
-        <h3>{heroH3[lang]}</h3>
+        <p className={classes["hero--p2"]}>{heroP2[lang]}</p>
+        <p className={classes["hero--p3"]}>{heroP3[lang]}</p>
         <a
           href="mailto:robbegarm@gmail.com?subject=Mail From Portfolio Website"
           className={`${classes["cta--link"]} link`}
