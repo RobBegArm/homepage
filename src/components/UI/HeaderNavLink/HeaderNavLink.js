dedecb210ed2e44e691ac14ae6a678991e8b698e
Robert Beglaryan <robbegarm@gmail.com>
2023-04-15 15:17:07 +0300

    Some Styling changes

--- src/components/UI/HeaderNavLink/HeaderNavLink.js
+++ src/components/UI/HeaderNavLink/HeaderNavLink.js
@@ -1,12 +1,12 @@
 import { SmoothScrollTo } from "../../../hooks/CustomHooks";
 
 const HeaderNavLink = (props) => {
+  const onClickAction = (e) => {
+    SmoothScrollTo(e, props.href);
+  };
+
   return (
-    <a
-      href={props.href}
-      onClick={(e) => SmoothScrollTo(e, props.href)}
-      className={"link"}
-    >
+    <a href={props.href} onClick={(e) => onClickAction(e)} className={"link"}>
       {props.title && <p>{props.title}</p>}
       {props.children}
     </a>
