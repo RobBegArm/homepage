f9839c3c2330eaad4b7c405ffb4aba5062c92dcc
Robert Beglaryan <robbegarm@gmail.com>
2023-04-17 13:55:22 +0300

    Updated Metadata, added 404 reroute

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
